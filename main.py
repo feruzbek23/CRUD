f5477d8373e4de16bd293b19514a073b3f343fc6
feruzbek23 <feruzbek.dev@gmail.com>
2025-03-03 23:30:39 +0100

    Fix readme

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routers import task_router
 from database import Base, engine
-
+import uvicorn
 
 Base.metadata.create_all(bind=engine)
 
@@ -10,5 +10,7 @@ app = FastAPI()
 # Register the task router
 app.include_router(task_router.router)
 
-
+if __name__ == "__main__":
+    # This block will only execute if the script is run directly, not when imported as a module
+    uvicorn.run(app, host="0.0.0.0", port=8080)
   
